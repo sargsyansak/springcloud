ce939257b5dc3e9a7de1f6dbcdbc77d4f5cd12c7
sako01 <sargsyansak@gmail.com>
2019-10-08 18:24:07 +0400

    fix maven version

--- develop/src/main/java/com/itspace/develop/DeepThoughtFeign.java
+++ develop/src/main/java/com/itspace/develop/DeepThoughtFeign.java
@@ -1,7 +1,7 @@
 package com.itspace.develop;
 
 import com.itspace.deepthought.DeepThought;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient("deep-thought")
 public interface DeepThoughtFeign  extends DeepThought {
